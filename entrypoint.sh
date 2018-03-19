9f321dcfb65af5e9a038bea6c2dcc2e42ab99c2d
Peter Stadler <stadlerpeter@yahoo.fr>
2018-03-19 16:13:19 +0100

    adding needed apache modules

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@
 cat <<EOF >> /usr/local/apache2/conf/httpd.conf
 LoadModule	ldap_module          modules/mod_ldap.so
 LoadModule	authnz_ldap_module   modules/mod_authnz_ldap.so
+LoadModule  proxy_module         modules/mod_proxy.so
+LoadModule  proxy_http_module    modules/mod_proxy_http.so
 <Location />
     ProxyPass  http://existdb:8080/
     ProxyPassReverse  http://existdb:8080/
